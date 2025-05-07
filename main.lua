818a4f6a5383f7e6d735417bbe58c7dbeb3ef228
Spacyxxuu <leroyevan01@gmail.com>
2025-05-07 22:08:29 +0200

    main.lua

--- main.lua
+++ main.lua
@@ -40,7 +40,7 @@ ScreenGui.DisplayOrder = 999999
 ScreenGui.IgnoreGuiInset = true
 
 Frame.Parent = ScreenGui
-Frame.BackgroundColor3 = Color3.fromRGB(17, 18, 20)
+Frame.BackgroundColor3 = Color3.fromRGB(100, 100, 100)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
 Frame.Position = UDim2.new(0.0883081928, 0, 0.110332131, 0)
@@ -60,8 +60,8 @@ TextLabel.BorderSizePixel = 0
 TextLabel.Position = UDim2.new(0.571090877, 0, 0.218952939, 0)
 TextLabel.Size = UDim2.new(0, 250, 0, 50)
 TextLabel.Font = Enum.Font.Nunito
-TextLabel.Text = "ZYPHERION"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Text = "SPACYXX"
+TextLabel.TextColor3 = Color3.fromRGB(65, 18, 99)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
@@ -75,13 +75,41 @@ TextLabel_2.Position = UDim2.new(0, 0, 0.61860466, 0)
 TextLabel_2.Size = UDim2.new(0, 300, 0, 200)
 TextLabel_2.Font = Enum.Font.Nunito
 TextLabel_2.Text = ""
-TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextColor3 = Color3.fromRGB(65, 18, 99)
 TextLabel_2.TextScaled = true
 TextLabel_2.TextSize = 14.000
 TextLabel_2.TextTransparency = 0.400
 TextLabel_2.TextWrapped = true
 
+Arsenal.Name = "Arsenal"
+Arsenal.Parent = Frame
+Arsenal.BackgroundColor3 = Color3.fromRGB(65, 18, 99)
+Arsenal.BackgroundTransparency = 0.900
+Arsenal.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Arsenal.BorderSizePixel = 0
+Arsenal.Position = UDim2.new(0.031160038, 0, 0.0357386284, 0)
+Arsenal.Size = UDim2.new(0, 300, 0, 70)
 
+ArsenalStroke.Parent = Arsenal
+ArsenalStroke.Color = Color3.fromRGB(26, 29, 37)
+ArsenalStroke.Thickness = 2
+ArsenalStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+
+UICorner_2.CornerRadius = UDim.new(0, 25)
+UICorner_2.Parent = Arsenal
+
+TextLabel_3.Parent = Arsenal
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(0.124573149, 0, 0, 0)
+TextLabel_3.Size = UDim2.new(0, 225, 0, 65)
+TextLabel_3.Font = Enum.Font.Nunito
+TextLabel_3.Text = "Arsenal"
+TextLabel_3.TextColor3 = Color3.fromRGB(65, 18, 99)
+TextLabel_3.TextSize = 48.000
+TextLabel_3.TextWrapped = true
 
 Loadbtn.Name = "Loadbtn"
 Loadbtn.Parent = Frame
@@ -92,7 +120,7 @@ Loadbtn.Position = UDim2.new(0.527222872, 0, 0.655968249, 0)
 Loadbtn.Size = UDim2.new(0, 320, 0, 50)
 Loadbtn.Font = Enum.Font.Nunito
 Loadbtn.Text = "Load"
-Loadbtn.TextColor3 = Color3.fromRGB(255, 255, 255)
+Loadbtn.TextColor3 = Color3.fromRGB(65, 18, 99)
 Loadbtn.TextScaled = true
 Loadbtn.TextSize = 14.000
 Loadbtn.TextWrapped = true
@@ -115,7 +143,126 @@ CloseButton.TextScaled = true
 CloseButton.TextSize = 14.000
 CloseButton.TextWrapped = true
 
-Universal.Name = "Agency"
+Planks.Name = "Planks"
+Planks.Parent = Frame
+Planks.BackgroundColor3 = Color3.fromRGB(17, 18, 20)
+Planks.BackgroundTransparency = 0.900
+Planks.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Planks.BorderSizePixel = 0
+Planks.Position = UDim2.new(0.031160038, 0, 0.219392911, 0)
+Planks.Size = UDim2.new(0, 300, 0, 70)
+
+PlanksStroke.Parent = Planks
+PlanksStroke.Color = Color3.fromRGB(26, 29, 37)
+PlanksStroke.Thickness = 2
+PlanksStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+
+UICorner_4.CornerRadius = UDim.new(0, 25)
+UICorner_4.Parent = Planks
+
+TextLabel_4.Parent = Planks
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(0.14229472, 0, -3.63304508e-07, 0)
+TextLabel_4.Size = UDim2.new(0, 225, 0, 70)
+TextLabel_4.Font = Enum.Font.Nunito
+TextLabel_4.Text = "Planks"
+TextLabel_4.TextColor3 = Color3.fromRGB(65, 18, 99)
+TextLabel_4.TextSize = 48.000
+TextLabel_4.TextWrapped = true
+
+Rivals.Name = "Rivals"
+Rivals.Parent = Frame
+Rivals.BackgroundColor3 = Color3.fromRGB(17, 18, 20)
+Rivals.BackgroundTransparency = 0.900
+Rivals.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Rivals.BorderSizePixel = 0
+Rivals.Position = UDim2.new(0.0311599784, 0, 0.419911742, 0)
+Rivals.Size = UDim2.new(0, 300, 0, 70)
+
+RivalsStroke.Parent = Rivals
+RivalsStroke.Color = Color3.fromRGB(26, 29, 37)
+RivalsStroke.Thickness = 2
+RivalsStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+
+UICorner_5.CornerRadius = UDim.new(0, 25)
+UICorner_5.Parent = Rivals
+
+TextLabel_5.Parent = Rivals
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(0.142294794, 0, -3.59030338e-07, 0)
+TextLabel_5.Size = UDim2.new(0, 225, 0, 70)
+TextLabel_5.Font = Enum.Font.Nunito
+TextLabel_5.Text = "Rivals"
+TextLabel_5.TextColor3 = Color3.fromRGB(265, 18, 99)
+TextLabel_5.TextSize = 48.000
+TextLabel_5.TextWrapped = true
+
+TextLabel_6.Parent = Frame
+TextLabel_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_6.BackgroundTransparency = 1.000
+TextLabel_6.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_6.BorderSizePixel = 0
+TextLabel_6.Position = UDim2.new(0.692038178, 0, 0.0561641678, 0)
+TextLabel_6.Size = UDim2.new(0, 60, 0, 75)
+TextLabel_6.Font = Enum.Font.Bangers
+TextLabel_6.Text = "Z"
+TextLabel_6.TextColor3 = Color3.fromRGB(65, 18, 99)
+TextLabel_6.TextScaled = true
+TextLabel_6.TextSize = 14.000
+TextLabel_6.TextWrapped = true
+
+TextLabel_7.Parent = TextLabel_6
+TextLabel_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_7.BackgroundTransparency = 1.000
+TextLabel_7.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_7.BorderSizePixel = 0
+TextLabel_7.Position = UDim2.new(0, 0, 0.61860466, 0)
+TextLabel_7.Size = UDim2.new(0, 300, 0, 200)
+TextLabel_7.Font = Enum.Font.Nunito
+TextLabel_7.Text = ""
+TextLabel_7.TextColor3 = Color3.fromRGB(65, 18, 99)
+TextLabel_7.TextScaled = true
+TextLabel_7.TextSize = 14.000
+TextLabel_7.TextTransparency = 0.400
+TextLabel_7.TextWrapped = true
+
+CounterBlox.Name = "CounterBlox"
+CounterBlox.Parent = Frame
+CounterBlox.BackgroundColor3 = Color3.fromRGB(17, 18, 20)
+CounterBlox.BackgroundTransparency = 0.900
+CounterBlox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+CounterBlox.BorderSizePixel = 0
+CounterBlox.Position = UDim2.new(0.0323932543, 0, 0.620120645, 0)
+CounterBlox.Size = UDim2.new(0, 300, 0, 70)
+
+CounterBloxStroke.Parent = CounterBlox
+CounterBloxStroke.Color = Color3.fromRGB(26, 29, 37)
+CounterBloxStroke.Thickness = 2
+CounterBloxStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+
+TextLabel_8.Parent = CounterBlox
+TextLabel_8.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_8.BackgroundTransparency = 1.000
+TextLabel_8.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_8.BorderSizePixel = 0
+TextLabel_8.Position = UDim2.new(0.142294794, 0, -3.59030338e-07, 0)
+TextLabel_8.Size = UDim2.new(0, 225, 0, 70)
+TextLabel_8.Font = Enum.Font.Nunito
+TextLabel_8.Text = "CounterBlox"
+TextLabel_8.TextColor3 = Color3.fromRGB(65, 18, 99)
+TextLabel_8.TextSize = 42.000
+TextLabel_8.TextWrapped = true
+
+UICorner_6.CornerRadius = UDim.new(0, 25)
+UICorner_6.Parent = CounterBlox
+
+Universal.Name = "Universal"
 Universal.Parent = Frame
 Universal.BackgroundColor3 = Color3.fromRGB(17, 18, 20)
 Universal.BackgroundTransparency = 0.900
@@ -124,7 +271,26 @@ Universal.BorderSizePixel = 0
 Universal.Position = UDim2.new(0.0312633105, 0, 0.821787298, 0)
 Universal.Size = UDim2.new(0, 300, 0, 70)
 
+UniversalStroke.Parent = Universal
+UniversalStroke.Color = Color3.fromRGB(26, 29, 37)
+UniversalStroke.Thickness = 2
+UniversalStroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
 
+TextLabel_9.Parent = Universal
+TextLabel_9.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_9.BackgroundTransparency = 1.000
+TextLabel_9.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_9.BorderSizePixel = 0
+TextLabel_9.Position = UDim2.new(0.142294794, 0, -3.59030338e-07, 0)
+TextLabel_9.Size = UDim2.new(0, 225, 0, 70)
+TextLabel_9.Font = Enum.Font.Nunito
+TextLabel_9.Text = "Universal"
+TextLabel_9.TextColor3 = Color3.fromRGB(65, 18, 99)
+TextLabel_9.TextSize = 48.000
+TextLabel_9.TextWrapped = true
+
+UICorner_7.CornerRadius = UDim.new(0, 25)
+UICorner_7.Parent = Universal
 
 TextLabel_10.Parent = Frame
 TextLabel_10.BackgroundColor3 = Color3.fromRGB(17, 18, 20)
@@ -133,7 +299,7 @@ TextLabel_10.BorderSizePixel = 0
 TextLabel_10.Position = UDim2.new(0.627807558, 0, 0.87694031, 0)
 TextLabel_10.Size = UDim2.new(0, 180, 0, 40)
 TextLabel_10.Font = Enum.Font.Nunito
-TextLabel_10.Text = "Thank you for using Spacyxx <3"
+TextLabel_10.Text = "Thank you for using Zypherion <3"
 TextLabel_10.TextColor3 = Color3.fromRGB(40, 58, 85)
 TextLabel_10.TextSize = 28.000
 
@@ -170,7 +336,29 @@ local function selectScript(scriptFrame, scriptName)
 	end
 end
 
+Arsenal.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		selectScript(Arsenal, "Arsenal")
+	end
+end)
 
+Planks.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		selectScript(Planks, "Planks")
+	end
+end)
+
+Rivals.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		selectScript(Rivals, "Rivals")
+	end
+end)
+
+CounterBlox.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		selectScript(CounterBlox, "Currently in Developement")
+	end
+end)
 
 Universal.InputBegan:Connect(function(input)
 	if input.UserInputType == Enum.UserInputType.MouseButton1 then
@@ -181,7 +369,16 @@ end)
 Loadbtn.MouseButton1Click:Connect(function()
 	if selectedOption then
 
-		if selectedOption == "Universal" then
+		if selectedOption == "Arsenal" then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20Arsenal%20Beta.lua"))()
+		elseif selectedOption == "Planks" then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/Zypherion%20Planks.lua"))()
+		elseif selectedOption == "Rivals" then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20Rivals%20Beta.lua"))()
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/Test.lua"))()
+		elseif selectedOption == "CounterBlox" then
+			loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20CounterBlox.lua"))()
+		elseif selectedOption == "Universal" then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/Games/ZYPHERION%20Universal.lua"))()
 		end
 	else
